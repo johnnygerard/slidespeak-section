c12ba09c35b26c0c633fc5d9ab3fe9744a848546
Johnny Gérard <john@jgerard.dev>
2026-02-05 00:40:35 +0100

    feat: add grab-cursor.svg icon to IconPDF component

--- src/components/icons/icon-pdf.tsx
+++ src/components/icons/icon-pdf.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import type { FC } from "react";
 import { tw } from "~/utils/tw";
 
@@ -17,7 +18,11 @@ export const IconPDF: FC<Props> = ({ className }) => (
         "0 -0.778px 0.778px 0 rgba(61, 61, 61, 0.08) inset, 0 6.224px 12.447px 0 rgba(82, 82, 82, 0.06), 0 3.112px 6.224px 0 rgba(82, 82, 82, 0.04), 0 1.556px 3.112px 0 rgba(82, 82, 82, 0.04), 0 1.556px 3.112px 0 rgba(228, 229, 231, 0.24)",
     }}
   >
-    {/* eslint-disable-next-line @next/next/no-img-element */}
     <img alt="" className="size-8" src="/assets/images/icons/pdf.svg" />
+    <img
+      alt=""
+      className="absolute -right-[0.91rem] -bottom-[0.78rem] size-8"
+      src="/assets/images/icons/grab-cursor.svg"
+    />
   </div>
 );
