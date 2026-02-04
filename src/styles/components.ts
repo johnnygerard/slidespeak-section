f96e791b847ed6cb4e259ffe6b6831a98c0f4024
Johnny Gérard <john@jgerard.dev>
2026-02-04 12:43:24 +0100

    feat: create Badge component

--- src/styles/components.ts
+++ src/styles/components.ts
@@ -3,3 +3,5 @@ import { tw } from "~/utils/tw";
 export const displayLgSemibold = tw([
   "text-5xl/tight font-semibold -tracking-[0.06rem]",
 ]);
+
+export const textSmSemibold = tw(["text-sm/5 font-semibold"]);
