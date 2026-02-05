ba7276f9124df4e95b4e5cff4c5a4c78301c97e8
Johnny Gérard <john@jgerard.dev>
2026-02-05 14:03:48 +0100

    feat: improve ProcessSection responsiveness

--- src/components/process-section.tsx
+++ src/components/process-section.tsx
@@ -61,7 +61,7 @@ export const ProcessSection: FC = () => (
       className={tw(
         sectionContainerStyles,
         "grid gap-6",
-        "grid-cols-1 md:grid-cols-2 lg:grid-cols-3",
+        "grid-cols-1 lg:grid-cols-2 xl:grid-cols-3",
       )}
     >
       {steps.map(({ content, title, description }, index) => (
